96900edbdd43c1ea28669208b39478b6fd636d3c
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2023-09-17 00:46:53 +1000

    Update sentences and animation

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const width = textContainer.offsetWidth
 const height = textContainer.offsetHeight
 const colCount = Math.floor(width / PixelsPerCharacter)
 const rowCount = Math.floor(height / PixelsPerLine)
+const longestSentenceLength = Math.max(...sentences.map((s) => s.length))
 
 const getUpdatedLines = (time) => {
   const outputLines = Array(rowCount).fill([""])
   for (let row = 0; row < rowCount; row++) {
     const rowsFromEdge = Math.min(row, rowCount - row)
+    const rowsFromCenter = Math.abs(row - rowCount / 2)
+    const rowSinFromEdge =
+      Math.sin(1 + time / 50 + rowsFromEdge / 10) * 0.55 + 0.7
     for (let col = 0; col < colCount; col++) {
-      outputLines[row] += getCharacter(row, col + time / col)
+      outputLines[row] += getCharacter(row, col * rowSinFromEdge)
     }
   }
   return outputLines.map((l) => l.replace(/ /g, "&nbsp;"))
@@ -21,7 +25,10 @@ const getUpdatedLines = (time) => {
 
 const getCharacter = (y, x) => {
   const sentence = sentences[sanitizeNumber(y) % sentences.length]
-  return sentence[sanitizeNumber(x) % (sentence.length + 40)] ?? " "
+  const longestDiff = longestSentenceLength - sentence.length
+  return (
+    sentence[sanitizeNumber(x) % (sentence.length + longestDiff + 20)] ?? " "
+  )
 }
 
 const sanitizeNumber = (n) => {
@@ -43,7 +50,7 @@ const mainLoop = async () => {
     const lines = getUpdatedLines(time)
     renderLines(lines)
     // Wait for next frame at 4ps
-    await new Promise((resolve) => setTimeout(resolve, 1000 / 8))
+    await new Promise((resolve) => setTimeout(resolve, 1000 / 17))
   }
 }
 
