5ef5c387f8e2e1c5c66441042c99bd1d99a8fb56
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2023-09-17 19:27:00 +1000

    More robust styling

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import sentences from "./sentences.js"
-
-const PixelsPerCharacter = 8
-const PixelsPerLine = 15
-const textContainer = document.getElementById("text_container")
-const width = textContainer.offsetWidth
-const height = textContainer.offsetHeight
-const colCount = Math.floor(width / PixelsPerCharacter)
-const rowCount = Math.floor(height / PixelsPerLine)
 const longestSentenceLength = Math.max(...sentences.map((s) => s.length))
+
+const textContainer = document.getElementById("text_container")
+const PixelsPerCharacter = 7.8
+const PixelsPerLine = 15
+let width, height, colCount, rowCount
+function onDisplaySizeUpdated() {
+  width = textContainer.offsetWidth
+  height = textContainer.offsetHeight
+  colCount = Math.floor(width / PixelsPerCharacter)
+  rowCount = Math.floor(height / PixelsPerLine)
+}
+window.addEventListener("resize", onDisplaySizeUpdated)
+onDisplaySizeUpdated()
 
 const getUpdatedLines = (time) => {
   const outputLines = Array(rowCount).fill([""])
@@ -15,9 +20,9 @@ const getUpdatedLines = (time) => {
     const rowsFromEdge = Math.min(row, rowCount - row)
     const rowsFromCenter = Math.abs(row - rowCount / 2)
     const rowSinFromEdge =
-      Math.sin(1 + time / 50 + rowsFromEdge / 10) * 0.55 + 0.7
+      Math.sin(1 + time / 100 + rowsFromEdge / 10) * 0.55 + 0.7
     for (let col = 0; col < colCount; col++) {
-      outputLines[row] += getCharacter(row, col * rowSinFromEdge)
+      outputLines[row] += getCharacter(row, col * rowSinFromEdge) ?? " "
     }
   }
   return outputLines.map((l) => l.replace(/ /g, "&nbsp;"))
@@ -26,9 +31,7 @@ const getUpdatedLines = (time) => {
 const getCharacter = (y, x) => {
   const sentence = sentences[sanitizeNumber(y) % sentences.length]
   const longestDiff = longestSentenceLength - sentence.length
-  return (
-    sentence[sanitizeNumber(x) % (sentence.length + longestDiff + 20)] ?? " "
-  )
+  return sentence[sanitizeNumber(x) % (sentence.length + longestDiff + 20)]
 }
 
 const sanitizeNumber = (n) => {
@@ -50,7 +53,7 @@ const mainLoop = async () => {
     const lines = getUpdatedLines(time)
     renderLines(lines)
     // Wait for next frame at 4ps
-    await new Promise((resolve) => setTimeout(resolve, 1000 / 17))
+    await new Promise((resolve) => setTimeout(resolve, 1000 / 60))
   }
 }
 
